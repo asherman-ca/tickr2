3962efa8ccf9dbcc7d9c559b7999fba7234c4b17
Alex Sherman <asherman.ca@gmail.com>
2022-12-06 14:09:28 -0800

    moneyparse dry up

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,26 +1,16 @@
 export const moneyParse = (num: number) => {
-	if (num > 1000000000) {
-		return (
-			(num / 1000000000).toLocaleString('en-US', {
-				style: 'currency',
-				currency: 'USD',
-				minimumFractionDigits: 2,
-			}) + 'B'
-		);
-	} else if (num > 1000000) {
-		return (
-			(num / 1000000).toLocaleString('en-US', {
-				style: 'currency',
-				currency: 'USD',
-				minimumFractionDigits: 2,
-			}) + 'M'
-		);
-	}
-	return num.toLocaleString('en-US', {
+	const options = {
 		style: 'currency',
 		currency: 'USD',
 		minimumFractionDigits: 2,
-	});
+	};
+
+	if (num > 1000000000) {
+		return (num / 1000000000).toLocaleString('en-US', options) + 'B';
+	} else if (num > 1000000) {
+		return (num / 1000000).toLocaleString('en-US', options) + 'M';
+	}
+	return num.toLocaleString('en-US', options);
 };
 
 export const numberParse = (num: number) => {
