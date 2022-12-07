ee05aba4809bd1791fb88626467c00ad88d9069f
Alex Sherman <asherman.ca@gmail.com>
2022-12-06 19:41:55 -0800

    trending elements complete

--- src/pages/Home/components/TrendingCard.tsx
+++ src/pages/Home/components/TrendingCard.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
+import { numberParse } from '../../../utils/numbers';
 import { coin } from '../../../utils/types';
+import styles from '../Home.module.css';
+const { trendingCoin, subHeader, trendingCoins, trendingCoinTitle } = styles;
 
 type trendingCardProps = {
 	coins: coin[];
@@ -13,14 +17,33 @@ const TrendingCard = ({ coins, title }: trendingCardProps) => {
 			<h3>
 				<i
 					className={`${
-						title.includes('Daily') ? 'fa-fire' : 'fa-calendar-week'
+						title.includes('Daily') ? 'fa-fire neg' : 'fa-calendar-week'
 					} fa-solid`}
 				></i>
 				{title}
 			</h3>
-			<div>
-				{coins.map((coin) => {
-					return <div>{coin.id}</div>;
+			<div className={`${trendingCoins}`}>
+				{coins.map((coin, idx) => {
+					return (
+						<div className={`${trendingCoin}`}>
+							<div>
+								<div className={`${subHeader}`}>{idx + 1}</div>
+								<div className={`${trendingCoinTitle}`}>
+									<img src={coin.image.thumb} alt='' />
+									{coin.id}
+								</div>
+							</div>
+							<div
+								className={
+									coin.market_data.price_change_percentage_24h > 0
+										? 'pos'
+										: 'neg'
+								}
+							>
+								{numberParse(coin.market_data.price_change_percentage_24h)}%
+							</div>
+						</div>
+					);
 				})}
 			</div>
 		</div>
