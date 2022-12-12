267140db7ab189027158fc8da6a375e521fea77c
Alex Sherman <asherman.ca@gmail.com>
2022-12-12 14:19:45 -0800

    home table init

--- src/pages/Home/components/CoinItem.tsx
+++ src/pages/Home/components/CoinItem.tsx
@@ -1,11 +1,32 @@
 import { coin } from '../../../utils/types';
+import styles from '../Home.module.css';
+const { coinItem, coinItemTitle } = styles;
 
 type CoinItemProps = {
 	coin: coin;
 };
 
 const CoinItem = ({ coin }: CoinItemProps) => {
-	return <div>{coin.id}</div>;
+	{
+		console.log('coin', coin);
+	}
+
+	return (
+		<div className={`${coinItem}`}>
+			<div className={`${coinItemTitle}`}>
+				<div>
+					<i className='fa-regular fa-star'></i>#
+				</div>
+				<div>{coin.name}</div>
+			</div>
+			<div>price</div>
+			<div>1h</div>
+			<div>24h</div>
+			<div>7d</div>
+			<div>Market Cap</div>
+			<div>Volume</div>
+		</div>
+	);
 };
 
 export default CoinItem;
